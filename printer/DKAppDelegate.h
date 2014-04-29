744c0617e76cd2e59704f85bdcda4efc1595e1ee
liudukun <liudukun@126.com>
2014-04-29 10:54:26 +0800

    Initial Commit

---
+++ printer/DKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DKAppDelegate.h
+//  printer
+//
+//  Created by imac_ldk on 14-4-29.
+//  Copyright (c) 2014年 ldk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ printer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  printer
+//
+//  Created by imac_ldk on 14-4-29.
+//  Copyright (c) 2014年 ldk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DKAppDelegate class]));
+    }
+}
